68b87ae27ef2dab75d4d4efbdecc8522a9cf6f55
arsley <blue20will@gmail.com>
2019-12-13 18:28:27 +0900

    :hammer::white_check_mark: Add cleanup&seed process before specs run

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,8 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+    Rails.application.load_seed
   end
 
   config.around(:each) do |example|
